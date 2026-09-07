178cc84520b571cc755744b9ffcca0d5631568ef
Onkar8010 <shendkaronkar@gmail.com>
2026-09-07 15:05:51 +0530

    implementation of Array to find the smallest and largest  element of array

--- Array3.cpp
+++ Array3.cpp
@@ -7,24 +7,27 @@ using namespace std;
 
 int main(){
     int size=7;
-      int nums[size];//={15,20,1,63,87,54,-45};
+      int nums[]={15,20,1,63,87,54,-45};
     
-
-        cout<<size<<endl;
-       for(int i=0;i<size;i++){
-        cin>>nums[i];
-
-       }
 
 
       int smallest = INT_MAX;
+      int largest = INT_MIN;
 
       for(int i=0; i < size; i++){
         if(nums[i] < smallest){
            smallest = nums[i];
         }
+
+        if(nums[i] > largest){
+            largest=nums[i];
+        }
+          
+
+
       }
       cout<<"smallest="<<smallest<<endl;
+       cout<<"largest="<<largest<<endl;
 
       return 0;
 
